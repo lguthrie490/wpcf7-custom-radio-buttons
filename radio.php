b6b0fa6c2dc988dc5806565506c6435be2e78929
Logan Guthrie <logan@lockdev.io>
2016-05-05 20:47:33 -0700

    On second thought, its probably a better idea to have the span after the input

--- radio.php
+++ radio.php
@@ -206,11 +206,11 @@ function wpcf7_radio_custom_shortcode_handler( $tag ) {
 		}
 
 		if ( $use_label_element && $extra_span_element ) {
-			$item = '<label><span class="contact-pseudo"></span>' . $item . '</label>'; 
+			$item = '<label>' . $item . '<span class="contact-pseudo"></span></label>'; 
 		} elseif ( $use_label_element ) {
 			$item = '<label>' . $item . '</label>';
 		} elseif ( $extra_span_element ) {
-			$item = '<span class="contact-pseudo"></span>' . $item;
+			$item = $item . '<span class="contact-pseudo"></span>';
 		}
 
 		if ( false !== $tabindex )
